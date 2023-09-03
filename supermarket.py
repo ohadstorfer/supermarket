fb2086229c1a48eb2b501bfa454ec194c3d94e6d
ohadstorfer <ohad.storfer@gmail.com>
2023-09-03 14:11:00 +0300

    <done for now>

--- supermarket.py
+++ supermarket.py
@@ -11,39 +11,36 @@ class Actions(Enum):
     PAY = 3
     EXIT = 4
 
-import json
-
 def load_products_from_json(filename):
     try:
         with open(filename, 'r') as file:
             products_data = json.load(file)
-            Products.extend(products_data)
-            # print("Products loaded successfully from JSON.")
+            return products_data
     except FileNotFoundError:
         print(f"File '{filename}' not found.")
     except json.JSONDecodeError:
         print(f"Error decoding JSON from '{filename}'.")
 
-class Product:
-    def __init__(self, name, price, stock) -> None:
-        self.name = name
-        self.price = price
-        self.stock = stock
+def save_products_to_json(filename, products):
+    try:
+        with open(filename, 'w') as file:
+            json.dump(products, file, indent=4)
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+    except json.JSONDecodeError:
+        print(f"Error decoding JSON from '{filename}'.")
 
-    def __str__(self) -> str:
-        return f"{self.name}, cost: {self.price}, in stock: {self.stock}"
 
 Cart = []
-Products = []
+Products = load_products_from_json("cart.JSON")
 
 def menu():
     while True:
-        global students
         while True:
             for action in Actions:
                 print(f'{action.value}: {action.name}')
 
-            moveON = True  
+            moveON = True
             while moveON:
                 user_input = input("Select action: ")
 
@@ -51,7 +48,7 @@ def menu():
                     user_input = int(user_input)
 
                     if 0 <= user_input <= 4:
-                        moveON = False  
+                        moveON = False
                     else:
                         print("Only numbers from 0 to 4 are allowed.")
                         logging.error(f'User entered an invalid number: {user_input}')
@@ -59,73 +56,68 @@ def menu():
                     print("Invalid input! Please enter a valid number.")
                     logging.error(f'User entered an invalid input: {user_input}')
 
-
-
             user_selection = Actions(user_input)
 
             if user_selection == Actions.PRINT_ALL_PRODUCTS:
                 for product in Products:
-                    print(product)
+                    print(f"{product['name']} - {product['price']}")
+
 
             if user_selection == Actions.ADD_TO_CART:
-                canMoveOn= True
+                canMoveOn = True
 
                 while canMoveOn:
                     selected_item_index = input("Select item: ")
                     if selected_item_index.isdigit():
                         selected_item_index = int(selected_item_index)
 
-                        if selected_item_index < 0 or selected_item_index > 3:
-                            print("Invalid input! Please choose a number between 0-3.")
-                            logging.error(f'User entered an invalid number: {selected_item_index}')
-                        else:
+                        if 0 <= selected_item_index < len(Products):
                             selected_product = Products[selected_item_index]
-                            print("item added to cart")
-                            canMoveOn=False
-
+                            if selected_product['availability'] >= 1:
+                                Cart.append(selected_product)
+                                selected_product['availability'] -= 1
+                                print("Item added to cart")
+                                logging.debug(f'User added {selected_product} to the cart ')
+                                canMoveOn = False
+                            else:
+                                print("Item is not available :(")
+                                logging.debug(f'User failed to add {selected_product} to the cart because the item is not available')
+                        else:
+                            print("Invalid input! Please choose a number between 0 and {len(Products) - 1}.")
+                            logging.error(f'User entered an invalid number: {selected_item_index}')
                     else:
                         print("Invalid input! Please enter a valid number.")
                         logging.error(f'User entered an invalid input: {user_input}')
 
-                if selected_product['availability'] >= 1:
-                    Cart.append(selected_product)
-                    logging.debug(f'User succeeded in adding {selected_product} to the cart')
-                else:
-                    print("Item is not available :(")
-                    logging.debug(f'User failed to add {selected_product} to the cart because the item is not available')
-
             if user_selection == Actions.SHOW_CART:
-                print(Cart)
-                total_cost = 0
                 for item in Cart:
-                    total_cost += item['price']
+                    print(f"{item['name']} - {item['price']}")
+                total_cost = sum(item['price'] for item in Cart)
                 print(f"Total cost of items in cart: ${total_cost}")
 
             if user_selection == Actions.PAY:
-                payment_method = input("Do you want to pay with cash or card? ")
-                if payment_method == "cash" or payment_method == "card":
-                    print("Thank you for buying here! Now go away!")
-                    logging.debug(f'User paid successfully')
-                    for item in Cart:
-                        for product in Products:
-                            if product["name"] == item["name"]:
-                                product["availability"] -= 1
-                    Cart.clear()
-                else:
-                    logging.error(f'User entered an invalid payment method: {payment_method}')
-                    print("Go away, you idiot!")
+                good = True
+                while good: 
+                    payment_method = input("Do you want to pay with cash or card? ")
+                    if payment_method == "cash" or payment_method == "card":
+                        print("Thank you for buying here! Now go away!")
+                        logging.debug(f'User paid successfully')
+                        for item in Cart:
+                            for product in Products:
+                                if product["name"] == item["name"]:
+                                    product["availability"] -= 1
+                        Cart.clear()
+                        save_products_to_json("cart.JSON", Products)
+                        good= False
+                    else:
+                        logging.error(f'User entered an invalid payment method: {payment_method}')
+                        print("Invalid input! Please enter only 'cash' or 'card' ")
 
             if user_selection == Actions.EXIT:
+                save_products_to_json("cart.JSON", Products)
                 return
 
+
+
 if __name__ == "__main__":
-    load_products_from_json("cart.JSON")
     menu()
-
-
-
-# רשימה למחר:
-# לדאוג לפוקצייה PAY 
-# כלומר שיהיה אפשר לנסות שוב להכניס ערך אחרי שהיוזר הכניס ערך לא נכון
-# להשמיש את הקובץ ג'ייסון ככה שהוא יתעדכן, כולל אחרי רכישה שיתעדכן המלאי
-# להעביר את כל הפונקציות לדף נפרד
